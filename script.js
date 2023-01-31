7dcd121410c987fdf4761edfb985bb3b20da7a60
jricardocam <jricardocam@gmail.com>
2023-01-30 19:28:42 -0600

    Añadiendo JS y modificando CSS

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function encriptar(){
+    document.getElementById("resultado").style.display = "none";
+    
+}
+
+
+function desencriptar(){
+    document.getElementById("resultado").style.display = "block";
+    
+}
